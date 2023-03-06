18d2c17c5d03e55fc79189868902b1ef898f3646
duckplasma <duck.plasma@gmail.com>
2023-03-06 00:26:46 -0500

    +String Updates to Paragraphs  > MSG_FirstStart, MSG_Statement, MSG_MenuOptions  > Border Strings cleaned up
    -Updated old WriteLines that use String format that could be updated with $ {var} format practice.

--- Program.cs
+++ Program.cs
@@ -4,8 +4,32 @@
 // * ====================================================================================================
 // * MESSAGES
 // * ====================================================================================================
-string MSG_FirstStart = "Welcome to Practice the Basics. This is a console app that shows of demonstrations of understanding some of the basics.";
-string MSG_Statement = " I really wanted to make this to learn how to program in C#. For the longest time I would just get by on fundemantals but never really dove deep into practical applications. \n Recently I left a project because there was a contract dispute. The thing that held me back from just making the game myself was the lack of understanding how to build complex systems. I want to learn to make games the right way without relying on other tooling. When I want to make something, I want to be able to at least get to a working prototype before giving up. I want to make this so that when my son is older, we can make cool games together and he can make his own video games. I have nothing but time, I have nothing but passion. Lets do this!.";
+
+string MSG_FirstStart = $@"Welcome to Practice the Basics. 
+This is a console app that shows of demonstrations of understanding some of the basics.";
+
+string MSG_Statement = $@"I really wanted to make this to learn how to program in C#.
+For the longest time I would just get by on fundemantals but never really dove deep into practical applications.
+Recently I left a project because there was a contract dispute. 
+The thing that held me back was the lack of understanding how to build complex systems and discipline.
+I want to learn to make games the right way without relying on if a package will be updated or not. 
+When I want to make something, I want to be able to at least get to a working prototype before giving up.
+
+I want to make this so that when my son is older, we can make cool games together and he can make his own video games.
+
+I have nothing but time, 
+I have nothing but passion. 
+
+Lets do this!.";
+
+string MSG_MenuOptions = $@"=-= MENU =-=
+1. Statement
+2. Options
+3. Examples
+4. Counter
+
+type 'exit' to exit the program.";
+
 string MSG_OptionsText = "Here are where options will be stored. For now there is nothing so the program will end.";
 string MSG_OptionHeaderText = "Please select an option from the list below. Using the Numpad to enter the number of the application you want to run.";
 string MSG_ExitMessage = "Bye.";
@@ -18,9 +42,9 @@ int OPTIONVALUE_SleepTimer = 200;
 // *
 // DECO
 // *
-string MSG_BorderEquals = "====================================================================================================";
-string MSG_BorderDash = "----------------------------------------------------------------------------------------------------";
-string MSG_BorderSpace = "                                                                                                    ";
+string MSG_BorderEquals = "[====]";
+string MSG_BorderDash = "[----]";
+string MSG_BorderSpace = "    ";
 // *
 // EXAMPLES
 // *
@@ -43,28 +67,23 @@ int ex_MSG_MathCounter = 0;
 // *
 // START
 // *
-Console.WriteLine(MSG_BorderEquals + "\n");
+Console.Clear();
 Console.WriteLine(MSG_FirstStart);
-Console.WriteLine(MSG_BorderEquals + "\n");
-//
-
 //ask Question
 Console.WriteLine(MSG_BorderDash + "\n");
 Console.WriteLine(MSG_OptionHeaderText);
 Console.WriteLine(MSG_BorderDash + "\n");
-
-Console.WriteLine("1. Statement");
-Console.WriteLine("2. Options");
-Console.WriteLine("3. Examples");
-Console.WriteLine("4. Counter");
-Console.WriteLine("Exit");
-
+Console.WriteLine(MSG_MenuOptions);
 Console.WriteLine(MSG_BorderDash);
 Console.WriteLine("\n\n\n\n");
 //
 MSG_Input = Console.ReadLine();
 MSG_Input = Convert.ToString(MSG_Input);
+
+// CLEAR SCREEN
+Console.Clear();
 //
+
 if (MSG_Input == "1")
 {
     Console.WriteLine(MSG_BorderDash + "\n");
@@ -81,19 +100,19 @@ else if (MSG_Input == "3") // # - Needs to up date to show all examples + needs 
 {
     Console.WriteLine(MSG_BorderDash + "\n");
     Console.WriteLine("String: [string varName = value]; ex:    string FirstName = \"Duck\";");
-    Console.WriteLine("^->: " + ex_MSG_String + "\n");
+    Console.WriteLine($"^->: {ex_MSG_String}\n");
     //
     Console.WriteLine("Int: [int varName = value]; ex:    int Age = 1000;");
-    Console.WriteLine("^->: " + ex_MSG_Int + "\n");
+    Console.WriteLine($"^->: {ex_MSG_Int}\n");
     //
     Console.WriteLine("Float (32) / Double (64) / Decimal (128) : EX VALUE IS 0.1234567890");
     Console.WriteLine("^->: " + ex_MSG_Float + "  [float ex_MSG_Float =  0.1234567890f;]\n" + "^->: " + ex_MSG_Double + "  [double ex_MSG_Double = 0.1234567890;]\n" + "^->: " + ex_MSG_Decimal + "  [decimal ex_MSG_Decimal = 0.1234567890m];\n");
     //
     Console.WriteLine("Char: [char varName = v]; ex:    char FirstInitial = \"D\";");
-    Console.WriteLine("^->: " + ex_MSG_Char + "\n");
+    Console.WriteLine($"^->: {ex_MSG_Char}\n");
     //
     Console.WriteLine("Bool: [bool varName = true or false]; ex:    bool EarthIsFlat = false;");
-    Console.WriteLine("^->: " + ex_MSG_Bool + "\n");
+    Console.WriteLine($"^->: {ex_MSG_Bool}\n");
     //
     Console.WriteLine(MSG_BorderDash + "\n");
 }
@@ -107,7 +126,6 @@ else if (MSG_Input == "4")
 {
     Console.Clear();
     Console.WriteLine(MSG_BorderDash + "\n");
-    Console.WriteLine("Math Fun" + "\n");
     Console.WriteLine("\n\n Count to Ten \n\n");
     while (ex_MSG_MathCounter < 11)
     {
